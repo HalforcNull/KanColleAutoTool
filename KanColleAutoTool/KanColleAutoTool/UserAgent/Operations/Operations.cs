0bee61bf0e830da96739f15095d2384aed3f425c
Halforc Null <yaorunan@gmail.com>
2014-07-17 19:04:16 -0500

    User action framteworks

---
+++ KanColleAutoTool/KanColleAutoTool/UserAgent/Operations/Operations.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KanColleAutoTool.Operations.Actions
+{
+    class Operation
+    {
+        protected virtual void checkData()
+        {
+            return;
+        }
+
+        protected abstract void startExecute()
+        {
+            return;
+        }
+
+        protected virtual void queueNewOperation(UserAgent _user)
+        {
+
+        }
+
+        public void Start(UserAgent _user)
+        {
+            checkData();
+            startExecute();
+            queueNewOperation(_user);
+        }
+    }
+}
